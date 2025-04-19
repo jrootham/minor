6999ec07dc6a7c236ef4f15ddc8c82138b60f437
Jim Rootham <jrootham@gmail.com>
2025-04-19 17:06:16 -0400

    Stripped down first version

--- editor/src/topLevel.js
+++ editor/src/topLevel.js
@@ -2,24 +2,12 @@ import {reset, setClick, getElement, getContents} from "./renderer.js";
 import {START, EditNode, EditIdentifier} from "./node.js";
 import {Or, Literal, Text, Comment, Terminal, Identifier} from "./type.js";
 import {Table} from "./symbols.js";
-import {editSyntax, editType, editFormat, editSemantics} from "./edit.js";
 import {DisplayVisitor} from "./displayVisitor";
 
 export function setup() 
 {
 	setClick("newProgram", newProgram);
-	// setClick("editSyntax", editSyntax);
-	// setClick("editType", editType);
-	// setClick("editFormat", editFormat);
-	// setClick("editSemantics", editSemantics);
 }
-
-// setClick("makeDialogue", makeDialogue);
-// setClick("", );
-// setClick("", );
-// setClick("", );
-// setClick("", );
-// setClick("", );
 
 var editingRoot = null;
 export var editSymbols = null;
